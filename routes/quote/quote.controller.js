12fd895716c1844f89645cda99aa1d3586ddb5a7
ayush didwaniya <didwaniya.ayush@gmail.com>
2020-07-08 23:28:44 +0530

    financial fiesta backend initial commit

---
+++ routes/quote/quote.controller.js
@@ -0,0 +1,51 @@
+const { Quote, validate } = require('./quote.model');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+   const quotes = await Quote.find();
+   res.send(quotes);
+})
+ 
+router.post('/', async (req, res) => { 
+    const { error } = validate(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    let quote = new Quote({
+        writter: req.body.writter,
+        quotation: req.body.quotation
+    });
+    quote = await quote.save();
+    res.send('New quote SuccessFully Saved');
+}) 
+
+router.put('/:id', async (req, res) => {
+    const {error} = validate(req.body);
+    if( error ) return res.status(404).send(errors.details[0].message);
+    
+    let quote = await Quote.findByIdAndUpdate(req.params.id,
+    {   
+        writter: req.body.writter,
+        quotation: req.body.quotation
+    }, 
+    {   new : true });
+    
+    if( !quote ) return res.status(404).send('The quote with the given Id doesnt exist');
+ 
+    res.send(quote);
+})
+
+router.delete('/:id', async (req, res) => { 
+    let quote = await Quote.findByIdAndRemove(req.params.id);
+    console.log(quote);
+    if( !quote ) return res.status(404).send('The customer with the given Id doesnt exist');
+    res.send(quote);
+})
+/* 
+router.get('/:id', async (req, res) => { 
+    let quote = await Quote.findById(req.params.id);
+    if(!quote) return res.status(404).send('The quote with the given Id doesnt Exist');
+    res.send(quote);
+}) */
+
+module.exports = router;
---
+++ routes/quote/quote.model.js
@@ -0,0 +1,31 @@
+// for validation
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Quote = mongoose.model('Quote', new mongoose.Schema({
+    writter: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    quotation: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255
+    }
+ }));
+
+ 
+function validateQuote(quote) {
+    const schema = {
+        writter: Joi.string().min(5).max(50).required(),
+        quotation: Joi.string().min(5).max(255).required(),
+    };
+    
+    return Joi.validate(quote, schema);
+}
+
+exports.Quote = Quote;
+exports.validate = validateQuote;
