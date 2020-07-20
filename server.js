4bb6b1866ffa9feabf3f8f5f6460fe309b3a6a5f
ayush didwaniya <didwaniya.ayush@gmail.com>
2020-07-20 22:40:46 +0530

    'çode update'

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/users', require('./routes/users/users.controller'));
 // global error handler
 app.use(errorHandler);
 
-// start server
+// start server detail
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
